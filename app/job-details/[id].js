63d2d91443ee19013a511b2274d23043e1b0bb19
Milan Mazik <mazik1990@hotmail.com>
2023-11-23 18:12:52 +0100

    Job details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -20,6 +20,8 @@ import {
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
+const tabs = ["About", "Qualifications", "Responsibilites"];
+
 const jobDetails = () => {
   const params = useSearchParams();
   const router = useRouter();
@@ -29,6 +31,7 @@ const jobDetails = () => {
   });
 
   const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
 
   const onRefresh = () => {};
 
@@ -74,7 +77,11 @@ const jobDetails = () => {
                 companyName={data[0].employer_name}
                 Location={data[0].job_country}
               />
-              <JobTabs />
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
             </View>
           )}
         </ScrollView>
